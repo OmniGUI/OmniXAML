71a84a7c0735f83da043f5fa7db4ea292f3eefd5
Súper JMN <superjmn@outlook.com>
2015-04-20 18:38:03 +0200

    More tests

---
+++ OmniXaml/ObjectWriter/XamlObjectWriter.cs
@@ -0,0 +1,12 @@
+﻿namespace OmniXaml.ObjectWriter
+{
+    public class XamlObjectWriter : IXamlObjectWriter
+    {
+        public void WriteNode(IXamlReader reader)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object Result { get; }
+    }
+}
