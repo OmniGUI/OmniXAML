1d92ec893618cdfefe88c3846848534e3d1c01ee
José Manuel Nieto Sánchez <superjmn@outlook.com>
2015-06-11 20:10:17 +0200

    Fix!

---
+++ OmniXaml/OmniXaml/Builder/AssemblyNameConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace OmniXaml.Builder
+{
+    using System.Collections.Generic;
+
+    public class AssemblyNameConfig
+    {
+        private readonly string name;
+
+        public AssemblyNameConfig(string name)
+        {
+            this.name = name;
+        }
+
+        public XamlNamespace With(IEnumerable<ConfiguredAssemblyWithNamespaces> assemblyAndClrs)
+        {
+            return new XamlNamespace(name, new AddressPack(assemblyAndClrs));
+        }
+    }
+}
