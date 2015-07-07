d7b160e4fb38b289f5e970be75c065128e3ab01a
Súper JMN <superjmn@outlook.com>
2015-07-08 00:16:55 +0200

    Yet another switch!

--- OmniXaml/Tests/OmniXaml.Tests/XamlXmlLoaderTests/SpecialTestsClassicObjectAssembler.cs
+++ OmniXaml/Tests/OmniXaml.Tests/XamlXmlLoaderTests/SpecialTestsClassicObjectAssembler.cs
@@ -3,13 +3,14 @@ namespace OmniXaml.Tests.XamlXmlLoaderTests
     using Assembler;
     using Classes;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NewAssembler;
 
     [TestClass]
     public class SpecialTestsClassicObjectAssembler : SpecialTestsBase
     {
         protected override IXamlLoader CreateLoaderWithRootInstance(DummyClass dummy)
         {
-            return new XamlXmlLoader(new ObjectAssembler(WiringContext, new ObjectAssemblerSettings { RootInstance = dummy }), WiringContext);
+            return new XamlXmlLoader(new SuperObjectAssembler(WiringContext, new ObjectAssemblerSettings { RootInstance = dummy }), WiringContext);
         }
     }
 }
