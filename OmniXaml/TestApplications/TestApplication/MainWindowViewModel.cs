69713c794dec763aeb8c619be16452aa29bd42bb
José Manuel Nieto Sánchez <superjmn@outlook.com>
2015-07-30 20:58:12 +0200

    Fixed all images

--- OmniXaml/TestApplications/TestApplication/MainWindowViewModel.cs
+++ OmniXaml/TestApplications/TestApplication/MainWindowViewModel.cs
@@ -13,10 +13,10 @@
         {
             People = new Collection<PersonViewModel>()
             {
-                new PersonViewModel("Johnny", "Mnemonic", new Uri("Images/johnny_mnemonic.jpg", UriKind.Relative)),
+                new PersonViewModel("Johnny", "Mnemonic", new Uri("Images/johnny_mnemonic.png", UriKind.Relative)),
                 new PersonViewModel("Inspector", "Gadget", new Uri("Images/inspector_gadget.jpg", UriKind.Relative)),
                 new PersonViewModel("Tom", "Jones", new Uri("Images/tom_jones.jpg", UriKind.Relative)),
-                new PersonViewModel("Soap", "MacTavish", new Uri("Images/soap_mactavish.jpg", UriKind.Relative)),
+                new PersonViewModel("Soap", "MacTavish", new Uri("Images/soap_mactavish.png", UriKind.Relative)),
                 new PersonViewModel("Pino", "D'Angio", new Uri("Images/pino_dangio.jpg", UriKind.Relative))
             };
 
