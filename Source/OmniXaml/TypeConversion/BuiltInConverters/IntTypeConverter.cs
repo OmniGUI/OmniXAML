9caeecee9318526a15fc9e38d4f3e715312f3302
José Manuel Nieto Sánchez <superjmn@outlook.com>
2015-09-28 22:53:26 +0200

    Fix for IntTypeConverter and Tests for it.

--- Source/OmniXaml/TypeConversion/BuiltInConverters/IntTypeConverter.cs
+++ Source/OmniXaml/TypeConversion/BuiltInConverters/IntTypeConverter.cs
@@ -7,7 +7,26 @@
     {
         public object ConvertFrom(IXamlTypeConverterContext context, CultureInfo culture, object value)
         {
-            return int.Parse((string) value);
+            var str = value as string;
+
+            if (value is int)
+            {
+                return Convert.ToInt32(value);
+            }
+            if (value is long)
+            {
+                return Convert.ToInt32(value);
+            }
+            else if (str != null)
+            {
+                long v;
+                if (long.TryParse(str, NumberStyles.Integer, CultureInfo.InvariantCulture, out v))
+                {
+                    return (int)v;
+                }
+            }
+
+            throw new InvalidOperationException();
         }
 
         public object ConvertTo(IXamlTypeConverterContext context, CultureInfo culture, object value, Type destinationType)
@@ -22,7 +41,7 @@
 
         public bool CanConvertFrom(IXamlTypeConverterContext context, Type sourceType)
         {
-            return true;
+            return sourceType == typeof(string) || sourceType == typeof(long) || sourceType == typeof(int);
         }
     }
 }
---
+++ Source/Tests/OmniXaml.Tests/BuiltInTypeConverterTests/IntTypeConverterTests.cs
@@ -0,0 +1,50 @@
+namespace OmniXaml.Tests
+{
+    using System;
+    using System.Globalization;
+    using System.Reflection;
+    using Moq;
+    using TypeConversion;
+    using TypeConversion.BuiltInConverters;
+    using Xunit;
+    using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+    public class IntTypeConverterTests
+    {
+        private Mock<IXamlTypeConverterContext> contextMock;
+        private IntTypeConverter sut;
+
+        public IntTypeConverterTests()
+        {
+            var typeContextMock = new Mock<ITypeContext>();
+
+            contextMock = new Mock<IXamlTypeConverterContext>();
+            contextMock.Setup(context => context.TypeRepository).Returns(typeContextMock.Object);
+            sut = new IntTypeConverter();
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("-3", -3)]
+        [InlineData(4L, 4)]
+        public void ConvertFromTests(object value, int converted)
+        {
+            
+            var actual = sut.ConvertFrom(contextMock.Object, CultureInfo.CurrentCulture, value);
+            Assert.AreEqual(converted, actual);
+        }
+
+        [Theory]
+        [InlineData(typeof(string), true)]
+        [InlineData(typeof(int), true)]
+        [InlineData(typeof(long), true)]
+        [InlineData(typeof(bool), false)]
+        [InlineData(typeof(Assembly), false)]
+        public void CanConvertFromTests(Type type, bool canConvert)
+        {
+
+            var actual = sut.CanConvertFrom(null, type);
+            Assert.AreEqual(canConvert, actual);
+        }
+    }
+}
