69dbe929e2b2281728435d564353727ccb1eb8b8
José Manuel Nieto Sánchez <superjmn@outlook.com>
2015-10-17 22:19:14 +0200

    Corrected error message in exception that is thrown when there is more than one object at the root level.

--- Source/OmniXaml/ObjectAssembler/Commands/StartObjectCommand.cs
+++ Source/OmniXaml/ObjectAssembler/Commands/StartObjectCommand.cs
@@ -18,7 +18,7 @@ namespace OmniXaml.ObjectAssembler.Commands
         {
             if (StateCommuter.Level == 0)
             {
-                throw new XamlParseException("An object cannot start after level zero has been reached. This condition may indicate that there are more than one objects at the root level. Please, verify that there are only one root object.");
+                throw new XamlParseException("An object cannot start after level zero has been reached. This condition may indicate that there are more than one object at the Root Level. Please, verify that there is ONLY one root object.");
             }
 
             if (ConflictsWithObjectBeingConfigured)
