6e4c4f626f6503421727bd7696285f6d92a2be68
José Manuel Nieto Sánchez <superjmn@outlook.com>
2016-02-23 22:49:36 +0100

    ITopDownValueContext now exposes StoreInstances :D. Thanks Grokys!

--- Source/OmniXaml/ObjectAssembler/Commands/ITopDownValueContext.cs
+++ Source/OmniXaml/ObjectAssembler/Commands/ITopDownValueContext.cs
@@ -1,10 +1,12 @@
 namespace OmniXaml.ObjectAssembler.Commands
 {
+    using System.Collections.Generic;
     using Typing;
 
     public interface ITopDownValueContext
     {
         void Add(object instance, XamlType xamlType);
         object GetLastInstance(XamlType xamlType);
+        IReadOnlyCollection<StoredInstance> StoredInstances { get; }
     }
 }
