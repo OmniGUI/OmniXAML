bcb3758183511e2c10158dee7519980edfc3f1c6
José Manuel Nieto Sánchez <superjmn@outlook.com>
2016-11-22 19:28:34 +0100

    Fixes attribute based registration of xaml namespaces

--- OmniXaml.Services/AttributeBasedTypeDirectory.cs
+++ OmniXaml.Services/AttributeBasedTypeDirectory.cs
@@ -18,17 +18,21 @@
 
         private IEnumerable<XamlNamespace> GetNamespaces(IEnumerable<Assembly> assemblies)
         {
-            var xamlNamespaces = from a in assemblies
+            var routes = from a in assemblies
                 let attributes = a.GetCustomAttributes<XmlnsDefinitionAttribute>()
                 from byNamespace in attributes.GroupBy(arg => arg.XmlNamespace)
                 let name = byNamespace.Key
                 let clrNamespaces = byNamespace.Select(arg => arg.ClrNamespace)
-                select XamlNamespace.Map(name)
-                    .With(
-                        Route.Assembly(a)
-                            .WithNamespaces(clrNamespaces.ToArray()));
+                let configuredAssemblyWithNamespaces = Route.Assembly(a).WithNamespaces(clrNamespaces.ToArray())
+                select new {Ns = name, configuredAssemblyWithNamespaces};
 
-            return xamlNamespaces;
+            var nss = from route in routes
+                group route by route.Ns
+                into g
+                let ns = g.Select(arg => arg.configuredAssemblyWithNamespaces).ToArray()  
+                select XamlNamespace.Map(g.Key).With(ns);
+                                  
+            return nss;
         }
 
         public Type GetTypeByFullAddress(Address address)
