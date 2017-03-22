9cce51925543a28ab2ed838f5507f61139d8c8e5
José Manuel Nieto Sánchez <superjmn@outlook.com>
2017-03-22 21:37:35 +0100

    Sync

---
+++ OmniXaml.Tests/Rework/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using OmniXaml.Rework;
+using OmniXaml.Tests.Model;
+
+namespace OmniXaml.Tests.Rework
+{
+    public class Class1
+    {
+        public void NamescopeTest()
+        {
+            var sut = new LaPolla();
+            var node = sut.Inflate(new ConstructionNode(typeof(Window)));
+        }
+    }
+
+    public class LaPolla
+    {
+
+        public LaPolla()
+        {
+
+        }
+
+        public object Inflate(ConstructionNode constructionNode)
+        {
+            INewObjectBuilder innerBestia = new NewObjectBuilder(new SimpleInstanceCreator(), new SimpleValueConverter(), new NoChangesPipeline());
+            innerBestia.NodeInflated.Subscribe()
+            return innerBestia.Inflate(constructionNode);
+        }
+    }
+
+    internal class NoChangesPipeline : IValuePipeline
+    {
+        public void Process(object parent, Member member, MutablePipelineUnit mutablePipelineUnit)
+        {
+        }
+    }
+
+    internal class SimpleValueConverter : ISmartSourceValueConverter
+    {
+        public (bool, object) TryConvert(string strValue, Type desiredTargetType)
+        {
+            var convertFrom = TypeDescriptor.GetConverter(desiredTargetType).ConvertFrom(strValue);
+            return (true, convertFrom);
+        }
+    }
+
+    internal class SimpleInstanceCreator : ISmartInstanceCreator
+    {
+        public CreationResult Create(Type type, CreationHints creationHints)
+        {
+            var instance = Activator.CreateInstance(type);
+            return new CreationResult(instance, new CreationHints());
+        }
+    }
+}
