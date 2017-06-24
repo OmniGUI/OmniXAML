a31a988a2ca609b61959b37b7cf2ba534ccd16b5
José Manuel Nieto Sánchez <superjmn@outlook.com>
2017-06-24 09:48:01 +0200

    Refreshed tests

--- OmniXaml.Tests/ObjectBuilderTests/CreationFixture.cs
+++
@@ -1,8 +0,0 @@
-namespace OmniXaml.Tests.ObjectBuilderTests
-{
-    public class CreationFixture
-    {
-        public object Result { get; set; }
-        public BuildContext BuildContext { get; set; }
-    }
-}
